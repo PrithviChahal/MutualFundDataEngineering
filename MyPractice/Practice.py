1cae0fa7827562d163a03c56284ea787cfa68456
Prithvi <pc257773@gmail.com>
2026-07-18 09:53:12 +0530

    Day 3: EDA analysis and visualizations completed

--- MyPractice/Practice.py
+++
@@ -1,9 +0,0 @@
-a = {1,2,4,7,2,4,1,6,4,1,3,3}
-b = {2,1,2,5,6,4,5,4,1,3,6}
-
-
-print("Length of a is : ",len(a),a)
-print("Length of b is : ",len(b),b)
-
-missingvalue = b - a 
-print(missingvalue)
